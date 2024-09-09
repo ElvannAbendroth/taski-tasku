3424fbf2e0d1b8209a2feec5ddffe9bcc6ca6464
Elvann <elvann.abendroth@gmail.com>
2024-09-09 16:02:06 +0300

    ⭐clerk session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { auth } from '@clerk/nextjs/server'
+
 const tasks = [
   { id: 1, text: 'Something to do', done: false },
   { id: 2, text: 'Another little task', done: true },
@@ -8,22 +10,31 @@ const tasks = [
 ]
 
 export default function Home() {
+  const { userId } = auth()
+
   return (
     <div className="flex flex-col gap-10">
       <h1 className="text-2xl font-bold">Tasks</h1>
       <div className="grid sm:grid-cols-3 gap-6">
-        {tasks.map(task => {
-          return (
-            <div className="flex flex-col p-6 bg-purple-300 rounded-2xl aspect-square text-sm gap-4" key={task.id}>
+        {userId &&
+          tasks.map(task => {
+            return (
               <div
-                className={`size-4 border-foreground border rounded-full ${
-                  task.done ? 'bg-foreground' : ' bg-transparent'
-                }`}
-              ></div>
-              <p>{task.text}</p>
-            </div>
-          )
-        })}
+                className="flex flex-col p-6 bg-purple-300 rounded-2xl aspect-square text-sm gap-4 wrap"
+                key={task.id}
+              >
+                <div className="flex flex-wrap gap-2 items-center">
+                  <div
+                    className={`size-4 border-foreground border rounded-full ${
+                      task.done ? 'bg-foreground' : ' bg-transparent'
+                    }`}
+                  ></div>
+                  <p>{task.text}</p>
+                </div>
+              </div>
+            )
+          })}
+        {!userId && <div>Login to see tasks</div>}
       </div>
     </div>
   )
