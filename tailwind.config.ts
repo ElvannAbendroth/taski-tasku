1c79e21cdd14d5264fca6399514b63136af8351c
Elvann <elvann.abendroth@gmail.com>
2024-09-09 15:30:30 +0300

    ⭐setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: '#FFFFFF',
+        foreground: '#000000',
+        border: 'C6C6C6',
+      },
+      fontFamily: {
+        body: ['Lato'],
+        display: ['Poppins'],
+        mono: ['Roboto Mono'],
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
